bd0f3587f3799b15bd36c977e8e61105f00b6767
Ghalba Vieira <72039896+Ghalbavieira@users.noreply.github.com>
2025-01-30 00:47:46 +0000

    feat: remembering js in studies

---
+++ estudos-javascript/src/controle/if.js
@@ -0,0 +1,8 @@
+// O if é uma estrutura condicional que permite executar um bloco de código apenas se uma condição for verdadeira.
+
+
+if(condicional){
+    // Código executado se a condição for verdadeira
+} else {
+    // Código executado se a condição for falsa
+}
