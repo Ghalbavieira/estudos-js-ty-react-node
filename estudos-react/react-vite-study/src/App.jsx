ab5e7ca0f054f9cb5538437302ddce5ed1811747
Ghalba Vieira <72039896+Ghalbavieira@users.noreply.github.com>
2025-03-03 01:10:30 +0000

    feat: create section with sidebar and main tags

--- estudos-react/react-vite-study/src/App.jsx
+++ estudos-react/react-vite-study/src/App.jsx
@@ -1,18 +1,27 @@
 import {Post}  from "./components/post"
 import * as style from './assets/global.css'
 import {Header} from "./components/header/header"
+import styles from "./App.module.css"
 
 export default function App() {
 
   return (
     <>
-      <Header>
-        <h1>Hello World</h1>
-      </Header>
-      <Post 
-        author="Ghalba"
-        content="Helloo World"
-      />
+      <Header />
+      
+      <section className={styles.wrapper}>
+        <aside>
+            aside
+        </aside>
+
+        <main>
+          <Post 
+            author="Ghalba"
+            content="Helloo World"
+          />
+        </main>
+      </section>
+    
     </>
   )
 }
