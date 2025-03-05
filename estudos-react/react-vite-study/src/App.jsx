014239b2d4f6b7a6bfb792ac7ce073b670ae202f
Ghalba Vieira <72039896+Ghalbavieira@users.noreply.github.com>
2025-03-05 00:48:09 +0000

    feat: create posts

--- estudos-react/react-vite-study/src/App.jsx
+++ estudos-react/react-vite-study/src/App.jsx
@@ -4,6 +4,39 @@ import {Header} from "./components/header/header"
 import Sidebar from "./components/sidebar/sidebar"
 import styles from "./App.module.css"
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/ghalbaVieira.png",
+      name: "Ghalba Vieira",
+      role: "Web Developer",
+    },
+    content: [
+      {type: 'paragraph', content: "Fala Galerra"},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifolio' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+      { type: 'link', content: '#novoprojeto #nlw' },
+    ],
+    publishAt: new Date('2025-03-04 21:05:00'),
+  },
+  {
+    id: 3,
+    author: {
+      avatarUrl: "https://github.com/ghalbaVieira.png",
+      name: "isa Vieira",
+      role: "Web Developer",
+    },
+    content: [
+      {type: 'paragraph', content: "Fala Galerra"},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifolio' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+      { type: 'link', content: '#novoprojeto #nlw' },
+    ],
+    publishAt: new Date('2025-03-04 21:05:00'),
+  }
+]
+
 
 export default function App() {
 
@@ -15,10 +48,15 @@ export default function App() {
         <Sidebar />
 
         <main>
-          <Post 
-            author="Ghalba"
-            content="Helloo World"
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishAt={post.publishAt}
+              />
+            )
+          })}
         </main>
       </section>
     
--- estudos-react/react-vite-study/src/components/post/post.jsx
+++ estudos-react/react-vite-study/src/components/post/post.jsx
@@ -2,33 +2,42 @@ import React from "react"
 import styles from './post.module.css';
 import {Comments} from "../comments/comments"
 import {Avatar} from "../avatar/avatar"
+import { format, formatDistanceToNow} from "date-fns";
+import ptBR from 'date-fns/locale/pt-BR'
 
-export function Post(props)  {
-   
+export function Post({author, content, publishAt})  {
+    const publishedDate = format(publishAt, "d 'de' LLLL 'às' HH:mm 'h'", {
+        locale: ptBR,
+    });
+
+    const publishedDateRelative = formatDistanceToNow(publishAt, {
+        locale: ptBR,
+        addSuffix: true,
+    })
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
                     <Avatar hasBorder src="https://github.com/ghalbaVieira.png"/>
                     <div className={styles.authorInfo}>
-                        <h4>{props.author}</h4>
-                        <p>Web Developer</p>
+                        <h4>{author.name}</h4>
+                        <p>{author.role}</p>
                     </div>
                 </div>
 
-                <time title=" " dateTime="">Publicado há 1h</time>
+                <time title={publishedDate} dateTime={publishAt.toISOString()}>{publishedDateRelative}</time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala Galerra </p>
-                <p>Acabei de subir mais um projeto no meu portifolio</p>
-                <p>
-                    <a> jane.design/doctorcare</a>
-                </p>
-                <p>
-                    <a>#novoprojeto</a> 
-                    <a>#nlw</a> 
-                </p>
+                {content.map((line, index) => {
+                    if (line.type === 'paragraph') {
+                        return <p key={index}>{line.content}</p>;
+                    } else if (line.type === "link") {
+                        return <p key={index}><a href="#">{line.content}</a></p>;
+                    }
+                    return null;
+                })}
             </div>
 
             <form className={styles.comentForm}>
